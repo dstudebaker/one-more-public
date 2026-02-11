f9a1020041bbc8a27fae957859b1683e5073a764
Your Name <your-email@example.comgit>
2026-02-11 15:17:39 -0800

    Fix amplify config + callback

---
+++ app/amplify.ts
@@ -0,0 +1,23 @@
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(
+  {
+    Auth: {
+      Cognito: {
+        // We'll fill these env vars in the next step
+        userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+        userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
+        loginWith: {
+          oauth: {
+            domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN!,
+            scopes: ["openid", "email", "profile"],
+            redirectSignIn: [process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGNIN!],
+            redirectSignOut: [process.env.NEXT_PUBLIC_COGNITO_REDIRECT_SIGNOUT!],
+            responseType: "code",
+          },
+        },
+      },
+    },
+  },
+  { ssr: true }
+);
